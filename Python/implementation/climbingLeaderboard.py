617535a69039f189696f42eb88a6784be7bc3ccf
José Luis Velázquez Rodríguez <56319230+joseluisvr93@users.noreply.github.com>
2022-02-10 20:45:36 -0600

    climbing

--- Python/implementation/climbingLeaderboard.py
+++ Python/implementation/climbingLeaderboard.py
@@ -1,14 +1,26 @@
 def climbingLeaderboard(ranked, player):
-    # Write your code here
     ranks = []
-    ranked = sorted(list(set(ranked)),reverse=True)
-    print(ranked)
-    for i in range(len(player)):
-        ranked.append(player[i])
-        ranked = sorted(list(set(ranked)),reverse=True)
-        rank = ranked.index(player[i])
-        ranks.append(rank + 1)
-    print(ranks)
+    player.reverse()
+    rank = 1
+    i = 0
+    j = 0
+    while i < len(player) or j < len(ranked):
+        if i < len(player) and j < len(ranked):
+            if player[i] >= ranked[j]:
+                ranks.append(rank)
+                i += 1
+            else:
+                if j+1 < len(ranked):
+                    if ranked[j] != ranked[j+1]:
+                        rank += 1
+                j += 1
+        elif i >= len(player) and j < len(ranked):
+            j+=1
+        elif j >= len(ranked) and i < len(player):
+            ranks.append(rank+1)
+            i+=1    
+
+    ranks.reverse()
     return ranks
 
 
