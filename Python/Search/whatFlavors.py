90f60f613a0258c3a095ab446ce2b6fed7b6abd7
José Luis Velázquez Rodríguez <joseluisvelazquezro@gmail.com>
2022-05-16 16:20:42 -0500

    update

--- Python/Search/whatFlavors.py
+++ Python/Search/whatFlavors.py
@@ -1,13 +1,25 @@
 def whatFlavors(cost, money):
     result = []
-    for i in range(len(cost)):
-        c2 = cost[i+1:len(cost)]
-        if money - cost[i] in c2:
-            j = c2.index(money - cost[i]) + i + 1
-            result.append(i+1)
-            result.append(j+1)
-            print(i+1, j+1)
-            return result
+    for sunny in cost:
+        johnny = money - sunny
+        if sunny != johnny:
+            if johnny in cost:
+                j = cost.index(johnny)
+                r1 =cost.index(sunny) + 1
+                r2 = j + 1
+                result.append(r1)
+                result.append(r2)
+                print(r1,r2)
+                return result
+        else:
+            if cost.count(sunny) > 1:
+                j = cost[cost.index(sunny) + 1:].index(johnny)
+                r1 = cost.index(sunny) + 1
+                r2 = j + 2 + cost.index(sunny)
+                result.append(r1)
+                result.append(r2)
+                print(r1,r2)
+                return result
 
 t = int(input())
 for t_itr in range(t):
